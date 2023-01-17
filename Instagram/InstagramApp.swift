41bca9a547d26c5a93aaed7320f10cec13fbb38b
apple <apple@MacBook-Pro.local>
2023-01-17 21:04:05 +0100

    initial commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by apple on 17/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
