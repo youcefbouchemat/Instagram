ef0bc9c290d228e6b157cbcbaf1cd4319317cddf
apple <apple@MacBook-Pro.local>
2023-01-23 14:33:54 +0100

    add profile and follow button in reel screen

--- Instagram/Components/ReelPlayer.swift
+++ Instagram/Components/ReelPlayer.swift
@@ -11,17 +11,49 @@ struct ReelPlayer: View {
     
     @Binding var reel: Reel;
     
+    private let profileImageDimension: CGFloat = 35;
+    
     var body: some View {
         ZStack{
             if let player = reel.player{
                 CustomVideoPlayer(player: player)
                 
                 VStack{
-                    HStack{
+                    
+                    HStack(alignment: .bottom){
                         
+                        VStack(alignment: .leading, spacing: 10){
+                            
+                            HStack(spacing: 15){
+                                
+                                Image("people_\(Int.random(in: 1..<9))")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: profileImageDimension,height: profileImageDimension)
+                                    .cornerRadius(profileImageDimension/2)
+                             
+                                Text("Youcef Bouchemat")
+                                    .font(.callout.bold())
+                                
+                                Button(action: {
+                                    
+                                }, label: {
+                                    Text("Follow")
+                                        .font(.caption.bold())
+                                })
+                            }
+                        }
                     }
-                }
+                }.frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .bottomLeading)
             }
         }
     }
 }
+
+/*
+struct ReelPlayer_Previews: PreviewProvider {
+    static var previews: some View {
+        ReelPlayer(reel: reel)
+    }
+}*/
+
